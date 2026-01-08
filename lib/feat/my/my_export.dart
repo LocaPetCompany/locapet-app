0b0070961d4d0388af815483af341dc1bdc1e4fe
Carl Lee <ckdwkdl91@gmail.com>
2026-01-08 10:24:07 +0900

    🚚 Chore: rename of top-level navigator

---
+++ lib/feat/my/my_export.dart
@@ -0,0 +1,4 @@
+// Data
+// Domain
+// Presentation
+export 'presentation/screen/my_screen.dart';
---
+++ lib/feat/nearby/nearby_export.dart
@@ -0,0 +1,4 @@
+// Data
+// Domain
+// Presentation
+export 'presentation/screen/nearby_screen.dart';
