bfe7e87c78d0f9a647e1bc5f2e0be6659af20ce0
Carl-Lee91 <ckdwkdl91@gmail.com>
2026-01-09 13:51:27 +0900

    🐛 Fix: fix router name and path

--- lib/feat/nearby/presentation/screen/nearby_screen.dart
+++ lib/feat/nearby/presentation/screen/nearby_screen.dart
@@ -2,8 +2,8 @@ import 'package:locapet/export/feat.dart';
 import 'package:locapet/export/package.dart';
 
 class NearbyScreen extends StatelessWidget {
-  static const String routeName = 'my';
-  static const String routePath = '/my';
+  static const String routeName = 'nearby';
+  static const String routePath = '/nearby';
 
   /// [NearbyScreen] 위젯을 생성합니다.
   const NearbyScreen({super.key});
