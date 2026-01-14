5781e7dfb669df70a86ba64f99bf01cfda936b03
Carl-Lee91 <ckdwkdl91@gmail.com>
2026-01-14 11:23:26 +0900

    ✨ Feat: add enroll pet screen

---
+++ lib/feat/auth/presentation/screen/enroll_pet_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EnrollPetScreen extends StatelessWidget {
+  static const String routeName = 'enroll_pet';
+  static const String routePath = '/enroll_pet';
+
+  const EnrollPetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
