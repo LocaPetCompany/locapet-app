2f9a197d6d47f4ba5d091642aea1134f210f2985
Carl-Lee91 <ckdwkdl91@gmail.com>
2026-01-14 14:47:03 +0900

    ✨ Feat: complete profile screen

--- lib/feat/auth/presentation/screen/create_profile_screen.dart
+++ lib/feat/auth/presentation/screen/create_profile_screen.dart
@@ -1,16 +1,89 @@
-import 'package:flutter/material.dart';
+import 'package:locapet/export/core.dart';
 import 'package:locapet/export/feat.dart';
+import 'package:locapet/export/package.dart';
 
 class CreateProfileScreen extends StatelessWidget {
   static const String routeName = 'create_profile';
-  static const String routePath = '/auth/create_profile';
+  static const String routePath = '/create_profile';
 
   const CreateProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CommonSafeArea(
-      child: Scaffold(appBar: SubAppBar(title: '프로필 설정')),
+    return CommonSafeArea(
+      child: BlocBuilder<CommonTextFieldCubit, CommonTextFieldState>(
+        builder: (context, textFieldState) {
+          return BlocBuilder<CreateProfileCubit, CreateProfileState>(
+            builder: (context, profileState) {
+              return Scaffold(
+                appBar: const SubAppBar(title: '프로필 설정'),
+                body: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 120.h,
+                        width: double.infinity,
+                        child: Center(
+                          child: GestureDetector(
+                            onTap: () =>
+                                context.read<CreateProfileCubit>().pickImage(),
+                            child: Stack(
+                              children: [
+                                profileState.profileImage != null
+                                    ? CommonDecoratedBox(
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          border: Border.all(
+                                            color: AppColor.grayColor200,
+                                            width: 1.54.w,
+                                          ),
+                                          image: DecorationImage(
+                                            image: FileImage(
+                                              profileState.profileImage!,
+                                            ),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        width: 86.w,
+                                        height: 86.h,
+                                      )
+                                    : SvgPicture.asset(
+                                        AssetPath.avatar,
+                                        width: 86.w,
+                                      ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: SvgPicture.asset(
+                                    AssetPath.camera,
+                                    width: 24.w,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Gap(24.h),
+                      const CommonTextField(),
+                    ],
+                  ),
+                ),
+                bottomSheet: BottomContainer(
+                  onTap: textFieldState.text.isNotEmpty
+                      ? () => context.pushNamed(EnrollPetScreen.routeName)
+                      : () {},
+                  text: '완료',
+                  boxColor: textFieldState.text.isNotEmpty
+                      ? AppColor.primaryColor500
+                      : AppColor.grayColor400,
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
