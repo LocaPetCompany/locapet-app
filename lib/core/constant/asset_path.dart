03a4f5f672d3493ba5bcc2f1a126ac6085766dfe
Carl-Lee91 <ckdwkdl91@gmail.com>
2026-01-14 10:48:04 +0900

    💄 Style: add svg asset and path

--- lib/core/constant/asset_path.dart
+++ lib/core/constant/asset_path.dart
@@ -9,8 +9,9 @@ class AssetPath {
   static const String onboardingThird = '$_imagePath/onboarding_third.png';
 
   // Svg
-  // Bottom navigation
-  static const String avatarActive = '$_svgIconPath/avatar_active.svg';
+  // Icon
+  static const String google = '$_svgIconPath/google.svg';
+  static const String apple = '$_svgIconPath/apple.svg';
 
   // Logo
   static const String logo = '$_svgLogoPath/main_logo.svg';
